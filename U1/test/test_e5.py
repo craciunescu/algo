0477828abfb83aaa5beb7137a6c9c3a5051670a1
Dave <david.craciunescu@edu.uah.es>
2020-02-18 18:08:32 +0100

    Figure out how Python Unit Testing works.

--- U1/test/test_e5.py
+++ U1/test/test_e5.py
@@ -5,17 +5,42 @@
     5. Program a function to determine if a number received as a parameter is
     prime. Analyze the efficiency and complexity of the provided solution(s).
 """
-
-# Benchmark imports.
-import unittest
-import sys
-sys.path.insert(1, '../src')
-import e5.is_prime_naive
+# Benchmarking
 from time import time
+
+# Testing
+import unittest
+from src import e5
 
 class TestPrimeMethods(unittest.TestCase):
     """ Test Methods in craciunescu@github.com/algo/U1/src/e5.py """
 
     def test_is_prime_naive(self):
-        self.assertTrue(is_prime_naive(797581))
+        """ Test is_prime_naive """
+        self.assertTrue(e5.is_prime_naive(797581))
+        self.assertTrue(e5.is_prime_naive(1049))
+        self.assertTrue(e5.is_prime_naive(23))
+        self.assertTrue(e5.is_prime_naive(2))
 
+        self.assertFalse(e5.is_prime_naive(7917))
+        self.assertFalse(e5.is_prime_naive(797582))
+        self.assertFalse(e5.is_prime_naive(-4))
+        self.assertFalse(e5.is_prime_naive(0))
+        self.assertFalse(e5.is_prime_naive(24))
+
+    def test_is_prime_optimized(self):
+        """ Test is_prime_optimized """
+
+        self.assertTrue(e5.is_prime_optimized(797581))
+        self.assertTrue(e5.is_prime_optimized(1049))
+        self.assertTrue(e5.is_prime_optimized(23))
+        self.assertTrue(e5.is_prime_optimized(2))
+
+        self.assertFalse(e5.is_prime_optimized(7917))
+        self.assertFalse(e5.is_prime_optimized(797582))
+        self.assertFalse(e5.is_prime_optimized(-4))
+        self.assertFalse(e5.is_prime_optimized(0))
+        self.assertFalse(e5.is_prime_optimized(24))
+
+if __name__ == '__main__':
+    unittest.main()
