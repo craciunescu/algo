564d9c1f5a890954c0da682a0ff1b357dffea7ad
Dave E <david.craciunescu@edu.uah.es>
2020-02-25 19:20:25 +0100

    Computer transfer

--- U2/test/test_e1.py
+++ U2/test/test_e1.py
@@ -18,9 +18,25 @@ from src import e1
 class TestMinPairMethods(unittest.TestCase):
     """ Test methods in craciunescu@github.com/algo/U2/src/e1.py """
 
-    def test_get_min_pair_sum(self):
+    def test_get_min_pair_sum01(self):
         """ Test get_min_pair_sum """
-        self.assertTrue(e1.get_min_pair_sum([5, 8, 4, 1, 7, 9]))
+        self.assertEqual(e1.get_min_pair_sum([5, 8, 4, 1, 7, 9]), )
+    
+    def test_get_min_pair_sum02(self):
+        """ Test get_min_pair_sum """
+        self.assertEqual(e1.get_min_pair_sum([5, 8, 4, 1, 7, 9]), )
+    
+    def test_get_min_pair_sum03(self):
+        """ Test get_min_pair_sum """
+        self.assertEqual(e1.get_min_pair_sum([5, 8, 4, 1, 7, 9]), )
+    
+    def test_get_min_pair_sum04(self):
+        """ Test get_min_pair_sum """
+        self.assertEqual(e1.get_min_pair_sum([5, 8, 4, 1, 7, 9]), )
+    
+    def test_get_min_pair_sum05(self):
+        """ Test get_min_pair_sum """
+        self.assertEqual(e1.get_min_pair_sum([5, 8, 4, 1, 7, 9]), )
 
 if __name__ == '__main__':
     unittest.main()
